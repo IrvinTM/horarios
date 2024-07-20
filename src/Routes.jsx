a83bf04c43c5f2074b20434cd23f6fe95849522d
IrvinTM <irvint698@gmail.com>
2024-07-19 18:32:27 -0600

    vr

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,33 +1,34 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import { ADS235 } from "./pages/ADS235";
-import {GPO135} from "./pages/GPO135"
+import { GPO135 } from "./pages/GPO135";
 import { IGE135 } from "./pages/IGE135";
 import { PDN135 } from "./pages/PDN135";
 import { PMM135 } from "./pages/PMM135";
+
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-    },
-    {
-        path: "/ads235",
-        element: <ADS235/>
-    },
-    {
-        path: "/gpo135",
-        element: <GPO135/>
-    },
-    {
-      path: "/ige135",
-      element: <IGE135/>
-    },
-    {
-      path: "/pdn135",
-      element: <PDN135/>
-    },
-    {
-      path: "/pmm135",
-      element: <PMM135/>
-    }
-  ]);
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/ads235",
+    element: <ADS235 />,
+  },
+  {
+    path: "/gpo135",
+    element: <GPO135 />,
+  },
+  {
+    path: "/ige135",
+    element: <IGE135 />,
+  },
+  {
+    path: "/pdn135",
+    element: <PDN135 />,
+  },
+  {
+    path: "/pmm135",
+    element: <PMM135 />,
+  },
+]);
